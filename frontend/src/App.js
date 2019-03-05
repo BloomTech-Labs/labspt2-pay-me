42ebcfa8640457fd1c22f10fe0ef747b4739c716
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-04 20:02:05 -0800

    Built out the basic form items required by the AddInvoice component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import AddInvoice from './components/AddInvoice';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <AddInvoice />
       </div>
     );
   }
