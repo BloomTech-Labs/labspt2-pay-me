1a280ae6194c47bbd23919fd9eab4e7d924ab773
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-05 19:30:05 -0800

    Changed over to class based component to maintain its own basic state.

--- frontend/src/components/AddInvoice.js
+++ frontend/src/components/AddInvoice.js
@@ -1,19 +1,39 @@
 import React from 'react';
 
-const AddInvoice = (props) => {
-    return (
-        <div>
-            <></> {/*Need the menu component here*/}
-            <form name='addInvoiceForm'>
-                <input className='addInvoiceForm-clientName' name='clientName'></input><br />
-                <input className='addInvoiceForm-companyName' name='companyName'></input><br />
-                <input className='addInvoiceForm-email' name='email'></input><br />
-                <input className='addInvoiceForm-phoneNumber' name='phoneNumber'></input><br />
-                <input className='addInvoiceForm-invoiceNumber' name='invoiceNumber'></input>
-                <button className='addInvoiceForm-uploadInvoice' name='uploadInvoice'>Upload Invoice</button>
-            </form>
-        </div>
-    )
+class AddInvoice extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            clientName: '',
+            companyName: '',
+            email: '',
+            phoneNumber: '',
+            invoiceNumber: '',
+            invoiceFilename: '',
+        }
+    }
+
+    changeValue = (event) => {
+        //console.log(`${event.target.name}:${event.target.value}`)
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form name='addInvoiceForm'>
+                    <input className='addInvoiceForm-clientName' name='clientName' onChange={this.changeValue} value={this.state.clientName}></input><br />
+                    <input className='addInvoiceForm-companyName' name='companyName' onChange={this.changeValue} value={this.state.companyName}></input><br />
+                    <input className='addInvoiceForm-email' name='email' onChange={this.changeValue} value={this.state.email}></input><br />
+                    <input className='addInvoiceForm-phoneNumber' name='phoneNumber' onChange={this.changeValue} value={this.state.phoneNumber}></input><br />
+                    <input className='addInvoiceForm-invoiceNumber' name='invoiceNumber' onChange={this.changeValue} value={this.state.invoiceNumber}></input>
+                    <button className='addInvoiceForm-uploadInvoice' name='uploadInvoice'>Upload Invoice</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default AddInvoice;
