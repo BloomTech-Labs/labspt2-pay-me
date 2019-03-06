20fdea96d4c8a7e78f798a36317791fccac7d9ea
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-05 19:38:28 -0800

    Added in the migrations directory to knexfile.js.

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,12 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/payMe.db'
+      filename: './dev.sqlite3'
     },
-    useNullAsDefault:true,
-    migrations:{
-      directory:'./data/migrations'
-    },
-    seeds:{
-      directory:'./data/seeds'
-    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    }
   },
 
   staging: {
