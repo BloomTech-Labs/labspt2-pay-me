14266d860d6249dd78532fd6971cebce56038cf0
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-05 20:04:18 -0800

    Finished up the rest of the form elements that this component should need.

--- frontend/src/components/AddInvoice.js
+++ frontend/src/components/AddInvoice.js
@@ -29,12 +29,20 @@ class AddInvoice extends React.Component {
         return (
             <div>
                 <form name='addInvoiceForm' onSubmit={this.submit}>
-                    <input className='addInvoiceForm-clientName' name='clientName' onChange={this.changeValue} value={this.state.clientName}></input><br />
-                    <input className='addInvoiceForm-companyName' name='companyName' onChange={this.changeValue} value={this.state.companyName}></input><br />
-                    <input className='addInvoiceForm-email' name='email' onChange={this.changeValue} value={this.state.email}></input><br />
-                    <input className='addInvoiceForm-phoneNumber' name='phoneNumber' onChange={this.changeValue} value={this.state.phoneNumber}></input><br />
-                    <input className='addInvoiceForm-invoiceNumber' name='invoiceNumber' onChange={this.changeValue} value={this.state.invoiceNumber}></input>
-                    <input type='file' name='invoice' className='addInvoiceForm-uploadInvoice'>Upload Invoice</input>
+                    <input className='addInvoiceForm-clientName' name='clientName' onChange={this.changeValue} value={this.state.clientName} />
+                    <br />
+                    <input className='addInvoiceForm-companyName' name='companyName' onChange={this.changeValue} value={this.state.companyName} />
+                    <br />
+                    <input className='addInvoiceForm-email' name='email' onChange={this.changeValue} value={this.state.email} />
+                    <br />
+                    <input className='addInvoiceForm-phoneNumber' name='phoneNumber' onChange={this.changeValue} value={this.state.phoneNumber} />
+                    <br />
+                    <input className='addInvoiceForm-invoiceNumber' name='invoiceNumber' onChange={this.changeValue} value={this.state.invoiceNumber} />
+                    <br />
+                    <label>Upload Invoice: </label>
+                    <input type='file' name='invoice' className='addInvoiceForm-uploadInvoice' />
+                    <br />
+                    <button name='submit'>Add Invoice</button>
                 </form>
             </div>
         )
