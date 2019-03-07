cd91b4a925643a7afc58f5da151103c060d4e5d6
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-06 18:49:52 -0800

    Setup base router files and applied them to the main server.js file.

--- api/server.js
+++ api/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 
+const users = require('./routes/users');
+const clients = require('./routes/clients');
+const invoices = require('./routes/invoices');
+const payments = require('./routes/payments');
+const reminders = require('./routes/reminders');
+
+
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/users', users)
+server.use('/api/clients', clients);
+server.use('/api/invoices', invoices);
+server.use('/api/payments', payments);
+server.use('/api/reminders', reminders);
 
 server.get('/', async (req, res) => {
     res.status(200).json({server: 'up'})
