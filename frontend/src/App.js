307089aeed1ba8600d316df5663de412edd39db2
Kendra Williams <kmonique.williams@gmail.com>
2019-03-13 20:01:16 -0400

    payinvoice.js set up with mock data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import React, { Component } from 'react';
 
-import {Elements, StripeProvider} from 'react-stripe-elements';
 import Home from './components/home/Home';
 import Contact from './components/contact/contact';
 import Signup from './components/authenicate/SignUp';
 import SignIn from './components/authenicate/SignIn';
-import Stripe from "./components/client/Stripe";
+import ClientStripe from "./components/client/Stripe";
 import { Route } from 'react-router-dom';
 
 // import AddInvoice from './components/AddInvoice';
-
-/*
-<StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
-        <div className="example">
-          <h1>Pay Invoice Example</h1>
-          <Elements>
-            <PayInvoice />
-          </Elements>
-        </div>
-</StripeProvider>
-*/
 
 class App extends Component {
   render() {
@@ -29,7 +17,7 @@ class App extends Component {
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/payinvoice" component={Stripe} />
+        <Route exact path="/payinvoice" component={ClientStripe} />
       </div>
     );
   }
