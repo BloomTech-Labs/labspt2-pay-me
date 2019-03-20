4a2967267010e0609f5df32a122d008af12ea4d7
Kendra Williams <kmonique.williams@gmail.com>
2019-03-20 00:01:25 -0400

    state successfully change to 0 on the front end for stripe, generic payment completed successfully

--- api/routes/charge.js
+++ api/routes/charge.js
@@ -1,22 +1,28 @@
 const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 const express = require("express");
 const router = express.Router();
+// const invoice = require(".../data/helpers/invoiceHelper.js");
 
 router.get("/", async (req, res) => {
     res.status(200).json({charge: "Up"});
 });
 
+// router.get("invoice", async (req, res) => {
+//    const client_id = req.params;
+//    const client_invoice = await invoice.findByClientId(client_id)
+//    res.status(200).json({invoice: client_invoice});
+// })
+
 router.post("/payment", async (req, res) => {
    const charge = req.body
-   const invoice = req.invoice
-   console.log(invoice)
    try {
-   //   let {status} = await stripe.charges.create({
-   //     amount: charge.amount,
-   //     currency: "usd",
-   //     description: `${charge.invoice}`,
-   //     source: charge.id
-   //   });
+      //change to include value pulled from the database
+     let {status} = await stripe.charges.create({
+       amount: 500,
+       currency: "usd",
+       description: `Test charge`,
+       source: charge
+     });
  
      res.json({status});
    } catch (err) {
