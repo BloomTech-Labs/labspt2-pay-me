50d0f96fbf9a2bb36d6cfb4fc82eedd429d70e8e
Jason Hedrick <jasonleehedrick@gmail.com>
2019-03-24 18:00:41 -0700

    Changed out the redirect on the server so it sends the user to the deployed frontend.

--- auth/google.js
+++ auth/google.js
@@ -5,13 +5,16 @@ const passportSetup = require('./passportConfig');
 const cors = require('cors');
 const JWT = require('./tokenGenerator');
 
+const local = 'http://localhost:3000/signin/';
+const deployed = 'https://keen-mestorf-44ec10.netlify.com/';
+const redirect = deployed;
 router.use(cors());
 
 router.get('/', passport.authenticate('google', {scope:['profile', 'email']}));
 
 router.get('/redirect', passport.authenticate('google', {session: false}), async (req, res) => {
     jwt = JWT.generateToken(req.user[0]);
-    res.redirect('http://localhost:3000/signin/' + jwt);
+    res.redirect(deployed + jwt);
 });
 
 module.exports = router;
