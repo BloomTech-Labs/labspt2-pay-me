407a238af43095ccc21492cbd9fc574764dff89a
Jason Hedrick <jasonleehedrick@gmail.com>
2019-04-02 19:03:30 -0700

    Have the AddInvoice component sending invoice data to the server and the server is adding it to the database.

--- api/routes/invoices.js
+++ api/routes/invoices.js
@@ -1,8 +1,50 @@
 const express = require('express');
+const invoices = require('../../data/helpers/invoiceHelper');
 const router = express.Router();
+
+/* 
+const multer = require('multer');
+const {google} = require('googleapis');
+const stream = require('stream');
+let storage = multer.memoryStorage();
+let upload = multer({ storage: storage });
+
+const drive = google.drive({
+    version: 'v3',
+    auth: 'oAuth2Client',
+}) */
+/* 
+async function uploadFile(stream, filename, mimetype) {
+    const res = await drive
+} 
+ */
+
+let invoiceObj = {
+    invoice_number: 1,
+    company_name: 'Test company',
+    inv_url: 'url/to/invoice.pdf',
+    notes: 'This is my jest test invoice',
+    client_id: 1
+};
+
 
 router.get('/', async (req, res) => {
     res.status(200).json({invoices: 'Up'});
+});
+
+router.post('/', async (req, res) => {
+    const data = req.body;
+    const invoice = {
+        invoice_number: data.invoice_number,
+        company_name: data.company_name,
+        inv_url: data.inv_url,
+        notes: '', // This needs to be fixed on the client side.
+        client_id: data.client_id,
+    };
+
+    const invoiceID = await invoices.insert(invoice);
+    console.log(invoiceID);
+    res.send(`Testing ${invoiceID}`);
 });
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,7 @@ import Contact from './components/contact/contact';
 import Signup from './components/authenicate/SignUp';
 import SignIn from './components/authenicate/SignIn';
 import ClientStripe from "./components/client/Stripe";
-
-// import AddInvoice from './components/AddInvoice';
+import AddInvoice from './components/addinvoice/AddInvoice';
 
 class App extends Component {
   render() {
@@ -20,7 +19,7 @@ class App extends Component {
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/payinvoice/:id" component={ClientStripe} />
-
+        <Route exact path="/invoices/add" component={AddInvoice} />
         <Route exact path="/signin/:jwt" render={(props) => <SignIn {...props}/>} />
         {/* <Route exact path="/reminders" component={Reminders} />*/}
       </div>
