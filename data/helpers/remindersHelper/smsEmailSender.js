60b86e94e0d07412025b5f3445c6dd8dd21d474b
Witanday <witanday.cd@gmail.com>
2019-04-06 20:00:34 -0400

    some changes on sms sender

--- data/helpers/remindersHelper/smsEmailSender.js
+++ data/helpers/remindersHelper/smsEmailSender.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
-<<<<<<< HEAD
 
 // yarn add nexmo  and then:
 const Nexmo = require('nexmo');
 
-=======
 // yarn add nexmo  and then:
 const Nexmo = require('nexmo');
 
 // yarn add @sendgrid/mail  and then:
 const sgMail = require('@sendgrid/mail');
 
+// yarn add nexmo  and then:
+const Nexmo = require('nexmo');
 
->>>>>>> cleaning
+
  // **Nexmo SMS API Quickstart for Node.JS**
 const smsHandler = (smsFromNumber, smstoNumber,smsText) =>{
 
@@ -44,12 +44,13 @@ console.log(smsFromNumber, smstoNumber, smsText);
 // **SENDGRID EMAIL API Quickstart for Node.JS**
 const emailHandler = (to,from,subject,html) =>{
 
-<<<<<<< HEAD
 // yarn add @sendgrid/mail  and then:
 const sgMail = require('@sendgrid/mail');
 
-=======
->>>>>>> cleaning
+// yarn add @sendgrid/mail  and then:
+const sgMail = require('@sendgrid/mail');
+
+
 /*To use this sample you will first need a SENDGRID account. Once you have your own API credentials, 
 create an .env file and set the value as required.*/
 
