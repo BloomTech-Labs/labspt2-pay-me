6a03dc4f3bb3d167b3a850d974c1112ffc9fee2d
Witanday <witanday.cd@gmail.com>
2019-04-07 19:01:53 -0400

    css styling

--- frontend/src/components/reminders/reminders.js
+++ frontend/src/components/reminders/reminders.js
@@ -298,21 +298,24 @@ submit = (event) => {
 
   <div class="col s12 m8 l10 "> 
   
-  <div class="col s12 m4 l3 ">{/*SEARCH INVOICE*/ }  
+  <div class="col s12 m4 l2 ">{/*SEARCH INVOICE*/ }  
   <div className="reminderInput boxShadow">
-    <SearchInput  onChange={this.searchUpdated} className='search'/>
+    <SearchInput  onChange={this.searchUpdated} className='search' />
         {filteredInvoice.map(itemInfo => {
           
         return (
             <div className="mail" key={itemInfo.invoiceId}>
-        <li className="info"  onClick={() => this.invoiceData(itemInfo.invoiceNumber)}>{itemInfo.clientInfo.clientName +'---'+ itemInfo.invoiceNumber}</li>
+        <li className="info active"  onClick={() => this.invoiceData(itemInfo.invoiceNumber)}>{itemInfo.clientInfo.clientName +'---'+ itemInfo.invoiceNumber}</li>
             </div>
                 )
               })}
             </div></div>    
   
-  <div class="col s12 m4 l9 Section-Email-Sms-Comment">{/*EMAIL&&SMS&&*/ }  
-  <div class="switch">
+  <div class="col s12 m4 l10 Section-Email-Sms-Comment">{/*EMAIL&&SMS&&*/ }  
+ 
+    <form className='Section-Email-Sms' onSubmit={this.handleStartReminders}>
+    <div className="switchbox">
+    <div className="switch">
     <label ><span>Email Status :</span>
       Off
       <input type="checkbox" value={this.state.isCheckedEmail} onChange={this.handleChangeActivEmail} />
@@ -327,8 +330,7 @@ submit = (event) => {
       <span class="lever"></span>
       On
     </label>
-  </div>
-    <form className='Section-Email-Sms' onSubmit={this.handleStartReminders}>
+  </div></div>
   <div className='Section-Email'>{/*&&EMAIL&&*/ } 
 
   <div className={`sectionboxcontact email${this.state.isCheckedEmail}`}>
@@ -337,19 +339,19 @@ submit = (event) => {
   
         </div>
             </div>
-         <div class="email-compose-body">
-         <h4 class=" mB-20">Send Email</h4>
+         <div class="email-compose-body boxwrapper">
+         <h4 class=" mB-20 rem">Send Email</h4>
          <div class="send-header"><div class="form-group">
          <div class="input-field col s4">
-          <span class="">Email_From:</span>
-          <input id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.userInfo.UserEmail}/>
+          <span class="rem">Email_From:</span>
+          <input  disabled id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.userInfo.UserEmail}/>
         </div>
          <div class="input-field col s4">
-          <span class="">Email_to:</span>
-          <input id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.clientInfo.clientEmail}/>
+          <span class="rem">Email_to:</span>
+          <input disabled id="icon_prefix" type="text" class="validate rem" value={this.state.invoiceUserClientInfo.clientInfo.clientEmail}/>
         </div>
         <div class=" col s4">
-          <span class="">Send first reminder on:</span>
+          <span class="rem">Send first reminder on:</span>
           <DatePicker
         selected={this.state.Email_StartDate}
         onChange={this.handleChangeDateEmail}
@@ -362,8 +364,8 @@ submit = (event) => {
         </div>
 
         <div class="input-field col s4">
-          <span class="">Then repeat :</span>
-          <Select 
+          <span class=" rem">Then repeat :</span>
+          <Select className='rem'
         value={this.state.selectedOption.value}
         onChange={this.handleChangeFreqEmail}
         options={options}
@@ -371,8 +373,8 @@ submit = (event) => {
         </div>
          </div>
 
-         <div class="form-group"><input class="form-control" name ="Email_Subject" value={this.state.Email_Subject} placeholder="Email Subject" onChange={this.handleInputChange2}/></div>
-         <div class="form-group"><textarea value={this.state.Email_CustomText} name="Email_CustomText" class="form-control" placeholder="Say Hi..." rows="10" onChange={this.handleInputChange2}></textarea></div>
+         <div class="form-group"><input class="form-control rem" name ="Email_Subject" value={this.state.Email_Subject} placeholder="Email Subject" onChange={this.handleInputChange2}/></div>
+         <div class="form-group"><textarea value={this.state.Email_CustomText} name="Email_CustomText" class="form-control rem" placeholder="Say Hi..." rows="10" onChange={this.handleInputChange2}></textarea></div>
          </div>
          <div id="compose-area"></div><div class="text-right mrg-top-30">
          </div>
@@ -388,11 +390,11 @@ submit = (event) => {
          <div class="send-header"><div class="form-group">
          <div class="input-field col s4">
           <span class="">Sms_From:</span>
-          <input id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.userInfo.UserPhoneNumber}/>
+          <input disabled id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.userInfo.UserPhoneNumber}/>
         </div>
          <div class="input-field col s4">
           <span class="">Sms_to:</span>
-          <input id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.clientInfo.clientPhoneNumber}/>
+          <input disabled id="icon_prefix" type="text" class="validate" value={this.state.invoiceUserClientInfo.clientInfo.clientPhoneNumber}/>
         </div>
         <div class=" col s4">
           <span class="">Send first reminder on:</span>
