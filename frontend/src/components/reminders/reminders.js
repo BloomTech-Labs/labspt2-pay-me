4a45fea5a2a32b7839de6ba56d13296b40aba399
Witanday <witanday.cd@gmail.com>
2019-04-07 00:54:28 -0400

    comment reminders link

--- frontend/src/components/reminders/reminders.js
+++ frontend/src/components/reminders/reminders.js
@@ -118,7 +118,6 @@ handleInputChange = ev => {
   this.setState({ [ev.target.name]: ev.target.value });
   ev.preventDefault();
 };
-<<<<<<< HEAD
 
 handleChangeDateSms=(date) =>{
   this.setState({
@@ -136,10 +135,6 @@ handleInputChange2 = ev => {
  
   ev.preventDefault();
 };
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
 handleChangeDateSms=(date) =>{
   this.setState({
@@ -170,7 +165,6 @@ submit = (event) => {
     /* We'll need to send this off to the AddInvoice endpoint on the server. */
 }
 
->>>>>>> b184c5cbdba58e96fa45a162f1f13bca140e1f64
 handleInputChange = ev => {
   this.setState({ [ev.target.name]: ev.target.value });
  
@@ -264,34 +258,6 @@ handleStartReminders = (e) => {
 
  
 //COMMENT
-<<<<<<< HEAD
-=======
-=======
->>>>>>> NavLeft minor change
-class Reminders extends Component {
-  constructor() {
-    super();
-    this.state = {
-        CustomInfo :[
-          {
-            Name: '',
-            InvoiceId:'',
-            Email:'',
-            EmailSubject:'',
-            EmailTextContent:'',
-            EmailSendingFrequency:'',
-            ReminderEmailStartDate:'',
-            ReminderCancel:'',
-            PhoneNumber:'',
-            SmsSendingFrequency:'',
-            ReminderSmsStartDate:'',
-            SmsTextContent:'',
-            InvoiceFileLink:'',
-          }
-        ] 
-     
-    }
-}
 
 changeValue = (event) => {
     //console.log(`${event.target.name}:${event.target.value}`)
@@ -304,12 +270,7 @@ submit = (event) => {
     event.preventDefault();
     /* We'll need to send this off to the AddInvoice endpoint on the server. */
 }
-<<<<<<< HEAD
->>>>>>> NavLeft minor change
-=======
->>>>>>> NavLeft minor change
-=======
->>>>>>> b184c5cbdba58e96fa45a162f1f13bca140e1f64
+
   render() {
     const filteredInvoice = InvoicesInfo.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
     return (
@@ -336,7 +297,6 @@ submit = (event) => {
   </div>
 
   <div class="col s12 m8 l10 "> 
-<<<<<<< HEAD
   
   <div class="col s12 m4 l3 ">{/*SEARCH INVOICE*/ }  
   <div className="reminderInput boxShadow">
@@ -351,22 +311,6 @@ submit = (event) => {
               })}
             </div></div>    
   
-=======
-  
-  <div class="col s12 m4 l3 ">{/*SEARCH INVOICE*/ }  
-  <div className="reminderInput boxShadow">
-    <SearchInput  onChange={this.searchUpdated} className='search'/>
-        {filteredInvoice.map(itemInfo => {
-          
-        return (
-            <div className="mail" key={itemInfo.invoiceId}>
-        <li className="info"  onClick={() => this.invoiceData(itemInfo.invoiceNumber)}>{itemInfo.clientInfo.clientName +'---'+ itemInfo.invoiceNumber}</li>
-            </div>
-                )
-              })}
-            </div></div>    
-  
->>>>>>> b184c5cbdba58e96fa45a162f1f13bca140e1f64
   <div class="col s12 m4 l9 Section-Email-Sms-Comment">{/*EMAIL&&SMS&&*/ }  
   <div class="switch">
     <label ><span>Email Status :</span>
@@ -375,7 +319,6 @@ submit = (event) => {
       <span class="lever"></span>
       On
     </label>
-<<<<<<< HEAD
   </div>
   <div class="switch">
     <label ><span>Sms Status :</span>
@@ -385,17 +328,6 @@ submit = (event) => {
       On
     </label>
   </div>
-=======
-  </div>
-  <div class="switch">
-    <label ><span>Sms Status :</span>
-      Off
-      <input type="checkbox" value={this.state.isCheckedSms} onChange={this.handleChangeActivSms} />
-      <span class="lever"></span>
-      On
-    </label>
-  </div>
->>>>>>> b184c5cbdba58e96fa45a162f1f13bca140e1f64
     <form className='Section-Email-Sms' onSubmit={this.handleStartReminders}>
   <div className='Section-Email'>{/*&&EMAIL&&*/ } 
 
