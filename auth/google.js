0fd059a7097a76ff8e21ccb393c3973b8aa98126
Jason Hedrick <jasonleehedrick@gmail.com>
2019-04-15 20:45:32 -0700

    Redeploy.

--- auth/google.js
+++ auth/google.js
@@ -12,7 +12,7 @@ router.get('/', passport.authenticate('google', {scope:['profile', 'email']}));
 router.get('/redirect', passport.authenticate('google', {session: false}), async (req, res) => {
     console.log(req.user[0]);
     jwt = JWT.generateToken(req.user[0]);
-    res.redirect('https://sleepy-coast-80160.herokuapp.com/signin/' + jwt);
+    res.redirect('https://keen-mestorf-44ec10.netlify.com/signin/' + jwt);
 });
 
 module.exports = router;
