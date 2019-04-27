63669b1b7fbbd94029132391a98fac3f0b4dde15
Witanday <witanday.cd@gmail.com>
2019-04-27 00:06:51 -0400

    added reminders routes

--- api/routes/reminders.js
+++ api/routes/reminders.js
@@ -4,10 +4,20 @@ const TaskManager = require('../../data/helpers/remindersHelper/TaskManager')
 
 // ** ROUTERS FOR OUR REMINDERS  ** //
 
+<<<<<<< HEAD
 // ## GET ALL INVOICES
 ReminderRouter.get('/',TaskManager.getInvoices);
 
 // ## Save A REMINDER
+=======
+// ## GET  REMINDERS
+RouterReminder.get('/view',TaskManager.getRemindersbyInvoiceNumber);
+
+//## GET ALL INVOICES BY USER ID
+RouterReminder.get('/reminders/invoices/:id',TaskManager.getInvoices);
+
+//## SAVE REMINDERS AND STOP RESEND
+>>>>>>> added reminders routes
 RouterReminder.post('/save',TaskManager.SaveReminder);
 
 // ## SEND A REMINDER
