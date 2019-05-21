104c27cc5cb38f6eb5e6469822a1d4231b0d5e3d
Witanday <witanday.cd@gmail.com>
2019-05-20 23:32:41 -0400

    enable deployed server again

--- frontend/src/serverLoc.js
+++ frontend/src/serverLoc.js
@@ -1,8 +1,8 @@
 const local = 'http://localhost:5000';
 const deployed = 'https://sleepy-coast-80160.herokuapp.com';
 
-//const serverAddress = deployed;
-const serverAddress = local;
+const serverAddress = deployed;
+//const serverAddress = local;
 
 
 module.exports = serverAddress;
